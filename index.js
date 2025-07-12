53c68357e04ba8a5022adec54db9d6a5ba9d0ab1
ArturSilv4 <artur.silv4@gmail.com>
2025-07-12 12:56:35 -0300

    agora vai

--- index.js
+++ index.js
@@ -1,20 +1,18 @@
-const express = require("express");
-const cors = require("cors");
-const dotenv = require("dotenv");
-const transactionsRoutes = require("./routes/transactions");
+const express = require('express');
+const cors = require('cors');
+const db = require('./db');
+const transactionsRoutes = require('./routes/transactions');
 
-dotenv.config();
+const app = express(); // <- isso tem que vir antes de qualquer uso do `app`
+
+app.use(cors());
+app.use(express.json());
 
 app.get('/', (req, res) => {
   res.send('API de Finanças está online ✅');
 });
 
-
-const app = express();
-app.use(cors());
-app.use(express.json());
-
-app.use("/transactions", transactionsRoutes);
+app.use('/transactions', transactionsRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
